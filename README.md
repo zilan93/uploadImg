a43f964309b34ba0a10edab2c168a89c8dfe1ca7
yangqin <zilan93@126.com>
2017-05-29 23:34:41 +0800

    图片上传并预览插件

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# 图片上传插件（兼容IE7）
-引用前需要先引用jQuery框架；
+# 图片上传jQuery插件（兼容IE8）
+用法如下：
+提供html容器
+```
+<div class="picBox"></div>
+```
+直接调用插件
+```
+$(".picBox").uploadImg();
+```
+可传入参数调用
+```
+$(".picBox").uploadImg({
+	"picNum": 1,//上传图片张数
+	"width": 100,//图片宽度
+	"height": 100//图片高度
+})
+```
--- uploadImg.js
+++ uploadImg.js
@@ -1,112 +1,125 @@
 /*
 *上传图片并本地预览插件
 *兼容IE8
-*上传单张图片和多张图片的情况
-*参数容器box
-*参数图片张数
-*参数预览图片尺寸
+*obj    ----上传图片容器
+*picNum ----上传图片的张数
+*width  ----图片宽度
+*height ----图片宽度
  */
-(function() {
-//初始化变量
-var i = 0;
+(function($) {
+	// 构造函数
+	function UploadImg(obj,opt) {
+		debugger;
+		this.obj = obj;
+		this.$obj = $(obj);
+		this.defaultOpt = {
+			"picNum": 1,
+			"width": 100,
+			"height": 100
+		};
+		this.num = 0;
+		this.options = $.extend({},this.defaultOpt,opt);
+		this.init();
+	};
 // 初始化html
-function initDom() {
+UploadImg.prototype.init = function() {
 	var html = '<input type="file" class="fileInput">'
 				+'<div><button type="button" class="uploadBtn">上传图片</button></div>'
 				+'<div class="previewBox"></div>';
-	return html;
+	this.$obj.append($(html));
+	this.bindEvent();
 }
-//检查图片格式
-function isImg(url) {
-	console.log(url);
-	return /.+\.(jpg|png|jpeg|gif)$/.test(url);
-}
-// 构造函数
-function UploadImg(obj,picNum,width,height) {
-	this.obj = obj;
-	this.picNum = picNum;
-	this.width = width;
-	this.height = height;
-	var html = initDom();
-	obj.append($(html));
-	$(".uploadBtn").click(function() {
+//绑定事件
+UploadImg.prototype.bindEvent = function() {
+	var self = this;
+	this.$obj.on("click.choose",".uploadBtn",function() {
 		$(".fileInput").trigger("click");
 	});
-	$(".fileInput").change(function() {
-		var that = this;
-		operationImg(that,picNum,width,height);
-	})
+	$(".fileInput").on("change.upload",function() {
+		self.operationImg(this);
+	});
+	this.$obj.off(".choose,.upload");
 }
-//兼容IE处理；
-function previewImgIE(obj,imgBox,width,height) {
+//检查图片格式
+UploadImg.prototype.isImg = function(url) {
+	var result = /.+\.(jpg|png|jpeg|gif)$/.test(url);
+	if(!result) {
+		alert("您选择的图片格式有误，请重新选择");
+		return false;
+	} else {
+		return true;
+	}
+},
+//添加预览图片到页面上
+UploadImg.prototype.addImgHtml = function(url) {
+	if(this.options.picNum == 1) {
+		if($(".previewBox")) {
+			$(".previewBox").html("<img src="+ url +" width="+ this.options.width +" height="+ this.options.height +">");
+		}
+	} else {
+		if($(".previewBox") && this.num < this.options.picNum) {
+			$(".previewBox").append("<img src="+ url +" width="+ this.options.width +" height="+ this.options.height +">");
+			this.num++;
+		}	
+	}
+},
+//兼容IE处理
+UploadImg.prototype.previewImgIE = function(obj) {
 	obj.select();
 	$(obj).blur();
 	if(document.selection) {
 		var url = document.selection.createRange().text;
-		if(isImg(url)) {
+		if(this.isImg(url)) {
 			var imgWrap = "<div class='imgWrap'></div>";
-			imgBox.append($(imgWrap));
+			if(this.options.picNum == 1) {
+				$(".previewBox").html($(imgWrap));
+			} else if(this.options.picNum > 1 && this.num < this.options.picNum) {
+				$(".previewBox").append($(imgWrap));
+				this.num++;
+			} else {
+				return;
+			};
 			$(".imgWrap").css({
-				"width":width,
-				"height":height,
+				"width":this.options.width,
+				"height":this.options.height,
 				"display":"inline-block",
 				"margin-right":"10px",
 				"*display":"inline",
 				"*zoom":1
 			});
-			$(".imgWrap").css("filter","progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod = scale,src=\""+url+"\")");
-		} else {
-			alert("您输入的图片格式有误，请重新输入");
-			return false;
+			$(".imgWrap:last").css("filter","progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod = scale,src=\""+url+"\")");
 		}
 	}
-}
-//上传图片操作；
-function operationImg(obj,picNum,width,height) {
-	if(obj.files && obj.files[0]) {
-		if(picNum == 1) {
-			var file = obj.files[0];
-			var reader = new FileReader();
-			reader.onload = function(evt) {
-				if(isImg(evt.target.result)) {
-					$(".previewBox").html("<img src="+evt.target.result+" width="+width+" height="+height+">");
-				} else {
-					alert("您输入的图片格式有误，请重新输入");
-					return false;
-				}
-			};
-			reader.readAsDataURL(file);
+},
+//正常处理
+UploadImg.prototype.previewImg = function(obj) {
+	var file = obj.files[0];
+	var self = this;
+	if(this.isImg(file.name)) {
+		var reader = new FileReader();
+		reader.onload = function(evt) {
+			self.addImgHtml(evt.target.result);	
 		};
-		if(picNum > 1) {
-			if(i < picNum) {
-				var file = obj.files[0];
-				var reader = new FileReader();
-				reader.onload = function(evt) {
-					if(isImg(evt.target.result)) {
-						$(".previewBox").append("<img src="+evt.target.result+" width="+width+" height="+height+">");
-					} else {
-						alert("您输入的图片格式有误，请重新输入");
-						return false;
-					}
-				};
-				reader.readAsDataURL(file);
-				i++;
-			}
-		}
+		reader.readAsDataURL(file);
 	} else {
-		//兼容IE处理
-		if(picNum == 1) {
-			previewImgIE(obj,$(".previewBox"),width,height);
-
-		};
-		if(picNum > 1) {
-			if(i < picNum) {
-				previewImgIE(obj,$(".previewBox"),width,height);
-				i++;
-			}
-		}
+		alert("您输入的图片格式有误，请重新输入");
+		return false;
+	}
+};
+//上传图片操作；
+UploadImg.prototype.operationImg = function(fileObj) {
+	if(fileObj.files && fileObj.files[0]) {
+		//html5 files API
+		this.previewImg(fileObj);
+	} else {
+		//兼容IE
+		this.previewImgIE(fileObj);
 	}
 }
-//返回构造函数；
-window['UploadImg'] = UploadImg;
-})();
+//绑定插件
+$.fn.uploadImg= function(options) {
+	return this.each(function() {
+		new UploadImg(this,options);
+	});
+}
+})(jQuery);
